c300c281373fe36b5bdf421142078629fa33e74e
RemiChavance <remi.chavance@orange.fr>
2023-03-16 15:04:05 +0100

    data service + create components

---
+++ src/app/components/pompe/pompe.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pompe',
+  templateUrl: './pompe.component.html',
+  styleUrls: ['./pompe.component.scss']
+})
+export class PompeComponent {
+
+}
---
+++ src/app/services/data.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  private URI = 'http://localhost:3000/';
+
+  constructor(private http: HttpClient) { }
+
+  create(litre: number): Observable<any> {
+    return this.http.post<any>(`${this.URI}/data`, { litre });
+  }
+
+  get(code: number): Observable<any> {
+    return this.http.get<any>(`${this.URI}/data/${code}`);
+  }
+
+  update(code: number, litre: number): Observable<any> {
+    return this.http.put<any>(`${this.URI}/data/${code}`, { litre });
+  }
+
+  delete(code: number): Observable<any> {
+    return this.http.delete<any>(`${this.URI}/data/${code}`);
+  }
+}
